ce3402ecd4fcca45839dce0fd2ccaf315bd6d58f
James Brundage <+@noreply.github.com>
2025-03-16 12:55:47 -0700

    fix: Updating Build Name ( re #3, re #4 )

--- Build/JSON-LD.GitHubWorkflow.PSDevOps.ps1
+++ Build/JSON-LD.GitHubWorkflow.PSDevOps.ps1
@@ -4,7 +4,7 @@ Import-BuildStep -SourcePath (
 ) -BuildSystem GitHubWorkflow
 
 Push-Location ($PSScriptRoot | Split-Path)
-New-GitHubWorkflow -Name "oEmbed Module Build" -On Push,
+New-GitHubWorkflow -Name "JSON-LD Module Build" -On Push,
     PullRequest, 
     Demand -Job  TestPowerShellOnLinux, 
     TagReleaseAndPublish, JsonLD -OutputPath .\.github\workflows\JSON-LD-Build.yml
